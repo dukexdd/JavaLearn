60e81094a6eb3e941e8fed7fc977991f2f7cf41b
dukexdd <dark.drexess@gmail.com>
2024-11-28 17:54:21 +0300

    Добавил условие - число от 1 до 100 в 4 задание

--- Практика 3/GuessTheNumber/src/Main.java
+++ Практика 3/GuessTheNumber/src/Main.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) {
 
         int value = new Random().nextInt(101);
-        int attempt = 0;
+        int attempt;
         System.out.println("Попытайтесь угадать число от 1 до 100: ");
 
         do {
             attempt = new Scanner(System.in).nextInt();
-
-            if ( attempt < value ) {
+        if (attempt <= 100 && attempt >= 1) {
+            if (attempt < value) {
                 System.out.println("Введенное число меньше загаданного!");
             } else if (attempt > value) {
                 System.out.println("Введенное число больше загаданного!");
             } else {
                 System.out.println("Вы угадали!");
             }
+        } else {
+            System.out.println("Введите число в указанном диапазоне!");
+        }
         } while (attempt != value);
 
     }
